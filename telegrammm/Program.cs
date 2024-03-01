84e541b94e150cfe754a94111aaf96e9794e21cd
Markul <markysikus@yandex.ru>
2024-03-01 12:08:23 +0700

    first commit

---
+++ telegrammm/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+namespace teleggg
+{
+    internal class Program
+    {
+        private static ITelegramBotClient _botClient;
+        private static ReceiverOptions _receiverOptions;
+
+        public static Task Main() => new Program().MainAsync();
+        public async Task MainAsync()
+        {
+            _botClient = new TelegramBotClient("none");
+            _receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = new[]
+                {
+                    UpdateType.Message,
+                },
+                ThrowPendingUpdates = true,
+            };
+            using var cts = new CancellationTokenSource();
+            _botClient.StartReceiving(UpdateHandler, ErrorHadler, _receiverOptions, cts.Token);
+            var me = await _botClient.GetMeAsync();
+            Console.WriteLine($"{me.FirstName} запущен!!!");
+            await Task.Delay(-1);
+        }
+        private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            try
+            {
+                switch (update.Type)
+                {
+                    case UpdateType.Message:
+                        {
+                        var message = update.Message;
+                            var user = message.From;
+                            Console.WriteLine($"{user.FirstName} ({user.Id}) написал сообщение:{message.Text}");
+                            var chat = message.Chat;
+                            switch(message.Type)
+                            {
+                                case MessageType.Text:
+                                    {
+                                        if(message.Text == "/start")
+                                        {
+                                            await botClient.SendTextMessageAsync
+                                                (chat.Id,
+                                                "Данный бот хуеглот"
+                                                );
+                                        }
+                                            return;
+                                    }
+                            }
+                            return;
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        private static Task ErrorHadler(ITelegramBotClient botClient, Exception error,CancellationToken cancellationToken)
+        {
+            var ErrorMessage = error switch
+            {
+                ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => error.ToString()
+            };
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+       
+    }
+}
+
+
+
